420862bcfd6b8f38a2229a279dc818e245224ecb
Hannes Ehrensperger <hannes.ehrensperger@helsana.ch>
2017-04-13 14:00:11 +0200

    Counter ohne rxjs/Observable

--- src/formapp/app/home/home.component.ts
+++ src/formapp/app/home/home.component.ts
@@ -12,68 +12,54 @@ import {Subscription} from 'rxjs';
     templateUrl: 'home.component.html'
 })
 
-export class HomeComponent implements OnDestroy {
+export class HomeComponent {
 
-    public source: Observable<any>;
-    private sourcesubscription: Subscription;
+
     private countUpDisp = 0;
     private countDownDisp = 60000;
-    private deinTick;
     private counterUp = 1;
     private counterDown = 60;
 
-    ngOnDestroy(): void {
-        this.sourcesubscription.unsubscribe();
-    }
-
-    countUp(): void {
-        console.log('Count Up läuft');
-        var counterUpMs = this.counterUp * 1000;
-        this.countUpDisp = counterUpMs;
-        this.counterUp++;
-    }
-
-    countDown(): void {
-        console.log('Count Down läuft');
-        var counterDownMs = this.counterDown * 1000;
-        this.countDownDisp = counterDownMs;
-        this.counterDown--;
-    }
-
     startZeit() {
-
-        console.log('GESTARTET');
-        this.deinTick = 'GESTARTET';
-
-        this.source = Observable.interval(1000)
-            .timeInterval();
-          //.tick(5);
-
-        this.sourcesubscription = this.source.subscribe(
-             (y) => {
-                console.log('MyTick ' + y.value + ' / ' + y.interval);
-                //this.countUpDisp = y.value * 1000;
-                this.countUpDisp = 0;
-                 this.countUp();
-                 this.countDownDisp = 60000;
-                this.countDown();
-            },
-            function (err) {
-                console.log('Error: ' + err);
-            },
-            () => {
-                console.log('Fertig :-)');
-                //this.countUpDisp = 'fertig';
-            }
-        );
+        console.log('Start Zeit');
+        setTimeout(alert('Start Zeit'), 3000);
     }
 
-    stopZeit(): void {
-        console.log('GESTOPPT');
-        this.deinTick = 'GESTOPPT';
-        this.sourcesubscription.unsubscribe();
+
+    stopZeit() {
+        console.log('Stop Zeit');
+        setTimeout(function () {
+            alert('Stop Zeit');
+        }, 3000);
     }
+
+
+    private timer_is_on = 0;
+    private counterDisplay = 0;
+    private counter = 0;
+
+    stopCount() {
+        this.timer_is_on = 0;
+    }
+
+    startCount() {
+        this.timer_is_on = 1;
+        setTimeout(this.temp, 1000);
+    }
+
+    continueTimer() {
+        console.log('aaaaa');
+        this.counter++;
+        this.counterDisplay = this.counter * 1000;
+        setTimeout(this.temp, 1000);
+    }
+
+    temp = () => {
+        if (this.timer_is_on === 1) {
+            //console.log('wwwwww');
+            this.continueTimer();
+        }
+    };
 
 
 }
-
